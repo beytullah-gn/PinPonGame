1c4e9ac15e81189d0a0762f7f1393228005156df
beytullah-gn <b-gnydn_@hotmail.com>
2022-01-31 13:18:27 +0300

    Main

---
+++ PinPonGame/PinPonGame/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿
+namespace PinPonGame
+{
+    partial class GameForm
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.playground = new System.Windows.Forms.Panel();
+            this.racket = new System.Windows.Forms.PictureBox();
+            this.ball = new System.Windows.Forms.PictureBox();
+            this.score_label = new System.Windows.Forms.Label();
+            this.points_label = new System.Windows.Forms.Label();
+            this.gameover_label = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.playground.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.racket)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // playground
+            // 
+            this.playground.Controls.Add(this.gameover_label);
+            this.playground.Controls.Add(this.points_label);
+            this.playground.Controls.Add(this.score_label);
+            this.playground.Controls.Add(this.ball);
+            this.playground.Controls.Add(this.racket);
+            this.playground.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.playground.Location = new System.Drawing.Point(0, 0);
+            this.playground.Name = "playground";
+            this.playground.Size = new System.Drawing.Size(800, 450);
+            this.playground.TabIndex = 0;
+            // 
+            // racket
+            // 
+            this.racket.BackColor = System.Drawing.Color.Black;
+            this.racket.Location = new System.Drawing.Point(236, 398);
+            this.racket.Name = "racket";
+            this.racket.Size = new System.Drawing.Size(200, 20);
+            this.racket.TabIndex = 0;
+            this.racket.TabStop = false;
+            // 
+            // ball
+            // 
+            this.ball.BackColor = System.Drawing.Color.Red;
+            this.ball.Location = new System.Drawing.Point(288, 227);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(30, 30);
+            this.ball.TabIndex = 1;
+            this.ball.TabStop = false;
+            // 
+            // score_label
+            // 
+            this.score_label.AutoSize = true;
+            this.score_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.score_label.Location = new System.Drawing.Point(12, 9);
+            this.score_label.Name = "score_label";
+            this.score_label.Size = new System.Drawing.Size(176, 55);
+            this.score_label.TabIndex = 2;
+            this.score_label.Text = "Score :";
+            // 
+            // points_label
+            // 
+            this.points_label.AutoSize = true;
+            this.points_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.points_label.Location = new System.Drawing.Point(185, 9);
+            this.points_label.Name = "points_label";
+            this.points_label.Size = new System.Drawing.Size(51, 55);
+            this.points_label.TabIndex = 3;
+            this.points_label.Text = "0";
+            this.points_label.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.points_label.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // gameover_label
+            // 
+            this.gameover_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.gameover_label.Location = new System.Drawing.Point(388, 46);
+            this.gameover_label.Name = "gameover_label";
+            this.gameover_label.Size = new System.Drawing.Size(353, 309);
+            this.gameover_label.TabIndex = 3;
+            this.gameover_label.Text = "GameOver!\r\n\r\nF1 - Restart Game\r\n\r\nEsc  Exit Game\r\n";
+            this.gameover_label.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // GameForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.playground);
+            this.Name = "GameForm";
+            this.Text = "PinPon Game";
+            this.Load += new System.EventHandler(this.GameForm_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.GameForm_KeyDown);
+            this.playground.ResumeLayout(false);
+            this.playground.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.racket)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel playground;
+        private System.Windows.Forms.Label gameover_label;
+        private System.Windows.Forms.Label points_label;
+        private System.Windows.Forms.Label score_label;
+        private System.Windows.Forms.PictureBox ball;
+        private System.Windows.Forms.PictureBox racket;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ PinPonGame/PinPonGame/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PinPonGame
+{
+    public partial class GameForm : Form
+    {
+        public int speed_left = 4;
+        public int speed_top = 4;
+        public int score = 0;
+
+        public GameForm()
+        {
+
+            InitializeComponent();
+            timer1.Enabled = true;
+            Cursor.Hide();
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.TopMost = true;
+            this.Bounds = Screen.PrimaryScreen.Bounds;
+            racket.Top = playground.Bottom - (playground.Bottom/10);
+            gameover_label.Left = (playground.Width / 2) - (gameover_label.Width / 2);
+            gameover_label.Top = (playground.Height / 2) - (gameover_label.Height / 2);
+            gameover_label.Visible = false;
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            racket.Left = Cursor.Position.X-(racket.Width/2);
+            ball.Left += speed_left;
+            ball.Top  += speed_top;
+            if(ball.Left <= playground.Left)
+            {
+                speed_left = -speed_left;
+
+            }
+            if (ball.Right >= playground.Right)
+            {
+                speed_left = -speed_left;
+
+            }
+            if (ball.Top <= playground.Top)
+            {
+                speed_top = -speed_top;
+
+            }
+            if (ball.Bottom >= playground.Bottom)
+            {
+                gameover_label.Visible = true;
+                timer1.Enabled = false;
+
+            }
+            if (ball.Bottom >= racket.Top && ball.Bottom <= racket.Bottom && ball.Left >= racket.Left && ball.Right <= racket.Right)
+            {
+                speed_top += 2;
+                speed_left += 2;
+                speed_top =- speed_top;
+                score += 1;
+                points_label.Text = score.ToString();
+                Random random = new Random();
+                playground.BackColor = Color.FromArgb(random.Next(100, 255), random.Next(100, 255),random.Next(100, 255));
+
+            }
+
+        }
+
+        private void GameForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Escape)
+            {
+                this.Close();
+            }
+            if (e.KeyCode == Keys.F1)
+            {
+                ball.Top = 50;
+                ball.Left = 50;
+                speed_left = 4;
+                speed_top = 4;
+                score = 0;
+                points_label.Text = "0";
+                timer1.Enabled = true;
+                gameover_label.Visible = false;
+                playground.BackColor = Color.White;
+            }
+        }
+
+        private void GameForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
